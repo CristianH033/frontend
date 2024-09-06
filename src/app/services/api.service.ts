aaaa226c936d4b6ea8c767fa2af374c982d628b2
Cristian David Home <cristian_david033@hotmail.com>
2024-09-06 16:59:43 -0500

    Update API Service.

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -28,11 +28,57 @@ export class ApiService {
     );
   }
 
+  updateReservation(
+    id: number,
+    reservation: Reservation
+  ): Observable<Reservation> {
+    return this.http.put<Reservation>(
+      `${this.apiUrl}/reservations/${id}`,
+      reservation
+    );
+  }
+
+  deleteReservation(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/reservations/${id}`);
+  }
+
   getServices(): Observable<Service[]> {
     return this.http.get<Service[]>(`${this.apiUrl}/services`);
   }
 
+  getService(id: number): Observable<Service> {
+    return this.http.get<Service>(`${this.apiUrl}/services/${id}`);
+  }
+
+  createService(service: Service): Observable<Service> {
+    return this.http.post<Service>(`${this.apiUrl}/services`, service);
+  }
+
+  updateService(id: number, service: Service): Observable<Service> {
+    return this.http.put<Service>(`${this.apiUrl}/services/${id}`, service);
+  }
+
+  deleteService(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/services/${id}`);
+  }
+
   getCustomers(): Observable<Customer[]> {
     return this.http.get<Customer[]>(`${this.apiUrl}/customers`);
+  }
+
+  getCustomer(id: number): Observable<Customer> {
+    return this.http.get<Customer>(`${this.apiUrl}/customers/${id}`);
+  }
+
+  createCustomer(customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>(`${this.apiUrl}/customers`, customer);
+  }
+
+  updateCustomer(id: number, customer: Customer): Observable<Customer> {
+    return this.http.put<Customer>(`${this.apiUrl}/customers/${id}`, customer);
+  }
+
+  deleteCustomer(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/customers/${id}`);
   }
 }

d85b53aabb670a4f54783baf2ed2322630006525
Cristian David Home <cristian_david033@hotmail.com>
2024-09-06 14:58:03 -0500

    Prime UI Scaffold.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, MenubarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend';
+  items: MenuItem[];
+
+  constructor() {
+    this.items = [
+      {
+        label: 'Inicio',
+        icon: 'pi pi-fw pi-home',
+      },
+      {
+        label: 'Productos',
+        icon: 'pi pi-fw pi-shopping-cart',
+      },
+      {
+        label: 'Contacto',
+        icon: 'pi pi-fw pi-envelope',
+      },
+    ];
+  }
 }
